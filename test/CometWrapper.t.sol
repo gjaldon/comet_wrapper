bc1a335c6fe114abe74e72ec0a0b1926d51196a4
Gabriel Jaldon <gjaldon85@gmail.com>
2023-02-14 12:40:49 +0800

    Tests for withdraw, deposit and totalAssets

---
+++ test/CometWrapper.t.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {Test} from "forge-std/Test.sol";
+import {CometWrapper, CometInterface} from "../src/CometWrapper.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import "forge-std/console.sol";
+
+address constant cometAddress = 0xc3d688B66703497DAA19211EEdff47f25384cdc3;
+address constant usdcHolder = 0x0A59649758aa4d66E25f08Dd01271e891fe52199;
+address constant usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+address constant cusdcHolder = 0x638e9ad05DBd35B1c19dF3a4EAa0642A3B90A2AD;
+
+contract CometWrapperTest is Test {
+    CometWrapper public cometWrapper;
+    CometInterface public comet;
+    ERC20 public usdc;
+
+    address alice = address(0xABCD);
+    address bob = address(0xDCBA);
+
+    function setUp() public {
+        vm.label(alice, "alice");
+        vm.label(bob, "bob");
+        vm.createSelectFork(vm.rpcUrl("mainnet"), 16617900);
+
+        usdc = ERC20(usdcAddress);
+        comet = CometInterface(cometAddress);
+        cometWrapper = new CometWrapper(ERC20(cometAddress), "Comet USDC", "cUSDCv3");
+
+        vm.prank(cusdcHolder);
+        comet.transfer(alice, 10_000e6);
+        assertGt(comet.balanceOf(alice), 999e6);
+
+        vm.prank(cusdcHolder);
+        comet.transfer(bob, 10_000e6);
+        assertGt(comet.balanceOf(bob), 999e6);
+    }
+
+    function test__totalAssets() public {
+        assertEq(cometWrapper.totalAssets(), 0);
+
+        vm.startPrank(alice);
+        comet.allow(address(cometWrapper), true);
+        cometWrapper.deposit(5_000e6, alice);
+        vm.stopPrank();
+
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+        vm.warp(block.timestamp + 14 days);
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+
+        vm.startPrank(bob);
+        comet.allow(address(cometWrapper), true);
+        cometWrapper.deposit(5_000e6, bob);
+        vm.stopPrank();
+
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+        vm.warp(block.timestamp + 14 days);
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+    }
+
+    function test__deposit() public {
+        vm.startPrank(alice);
+        comet.allow(address(cometWrapper), true);
+        cometWrapper.deposit(5_000e6, alice);
+        vm.stopPrank();
+
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+        // Account for rounding errors that lead to difference of 1
+        assertEq(cometWrapper.maxWithdraw(alice) - 1, comet.balanceOf(alice));
+
+        vm.warp(block.timestamp + 14 days);
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+        // Account for rounding errors that lead to difference of 1
+        assertEq(cometWrapper.maxWithdraw(alice) - 1, comet.balanceOf(alice));
+
+        vm.startPrank(bob);
+        comet.allow(address(cometWrapper), true);
+        cometWrapper.deposit(7_777e6, bob);
+        vm.stopPrank();
+
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+        uint totalAssets = cometWrapper.maxWithdraw(alice) + cometWrapper.maxWithdraw(bob);
+        // Alice and Bob should be able to withdraw all their assets without issue
+        assertLe(totalAssets, cometWrapper.totalAssets());
+    }
+
+    function test__withdraw() public {
+        vm.startPrank(alice);
+        comet.allow(address(cometWrapper), true);
+        cometWrapper.deposit(9_101e6, alice);
+        vm.stopPrank();            
+
+        vm.startPrank(bob);
+        comet.allow(address(cometWrapper), true);
+        cometWrapper.deposit(2_555e6, bob);
+        vm.stopPrank();
+
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+
+        vm.warp(block.timestamp + 14 days);
+        assertEq(cometWrapper.totalAssets(), comet.balanceOf(address(cometWrapper)));
+
+        vm.prank(alice);
+        cometWrapper.withdraw(173e6, alice, alice);
+        assertEq(cometWrapper.totalAssets() + 1, comet.balanceOf(address(cometWrapper)));
+
+        vm.warp(block.timestamp + 500 days);
+        assertEq(cometWrapper.totalAssets() + 1, comet.balanceOf(address(cometWrapper)));
+
+        uint totalAssets = cometWrapper.maxWithdraw(alice) + cometWrapper.maxWithdraw(bob);
+        assertLe(totalAssets, cometWrapper.totalAssets());
+
+        vm.startPrank(alice);
+        cometWrapper.withdraw(cometWrapper.maxWithdraw(alice), alice, alice);
+        vm.stopPrank();
+        
+        vm.startPrank(bob);
+        cometWrapper.withdraw(cometWrapper.maxWithdraw(bob), bob, bob);
+        vm.stopPrank();
+    }
+}
