64c42bfdb3334bc8e960b4b2c2fbc006de597cb2
Patrick McKelvy <pmckelvy1@gmail.com>
2023-02-20 22:57:06 -0500

    add test line to readme.

--- test/WrapperFuzz.t.sol
+++ test/WrapperFuzz.t.sol
@@ -8,7 +8,7 @@ import "./DSTestPlus.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
 import "../lib/forge-std/src/console.sol";
 
-contract WrapperTest is DSTestPlus {
+contract WrapperFuzzTest is DSTestPlus {
     address constant public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     ERC20 constant public CUSDC_V3 = ERC20(0xc3d688B66703497DAA19211EEdff47f25384cdc3);
     address constant public COMP = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
