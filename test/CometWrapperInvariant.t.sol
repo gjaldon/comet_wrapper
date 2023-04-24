375ec065ca0e0b80b518b8ec01487fe0b504ba63
Gabriel Jaldon <gjaldon85@gmail.com>
2023-04-24 11:03:26 +0800

    Different types of total balances must always match

--- test/CometWrapperInvariant.t.sol
+++ test/CometWrapperInvariant.t.sol
@@ -5,6 +5,58 @@ import {BaseTest, CometHelpers, CometWrapper, ERC20, ICometRewards} from "./Base
 
 contract CometWrapperInvariantTest is BaseTest {
     // Invariants:
+    // - totalAssets must always be <= comet.balanceOf(address(cometWrapper))
+    // - sum of all underlyingBalances of accounts <= totalAssets
+    // - sum of principal of all users == underlyingPrincipal
+    function test__contractBalanceInvariants(uint256 amount1, uint256 amount2) public {
+        vm.assume(amount1 <= 2**48);
+        vm.assume(amount2 <= 2**48);
+        vm.assume(amount1 + amount2 < comet.balanceOf(cusdcHolder));
+        vm.assume(amount1 > 1000e6 && amount2 > 1000e6);
+
+        vm.prank(cusdcHolder);
+        comet.transfer(alice, amount1);
+        vm.prank(cusdcHolder);
+        comet.transfer(bob, amount2);
+
+        uint256 aliceBalance = comet.balanceOf(alice);
+        vm.startPrank(alice);
+        comet.allow(wrapperAddress, true);
+        cometWrapper.deposit(aliceBalance/5, alice);
+        vm.stopPrank();
+        assertEq(comet.balanceOf(address(cometWrapper)), cometWrapper.totalAssets());
+
+        uint256 bobBalance = comet.balanceOf(bob);
+        vm.startPrank(bob);
+        comet.allow(wrapperAddress, true);
+        cometWrapper.deposit(bobBalance/5, bob);
+        vm.stopPrank();
+        assertEq(comet.balanceOf(address(cometWrapper)), cometWrapper.totalAssets());
+
+        skip(10000 days);
+
+        vm.startPrank(bob);
+        cometWrapper.mint(bobBalance/3, bob);
+        vm.stopPrank();
+        assertEq(comet.balanceOf(address(cometWrapper)), cometWrapper.totalAssets());
+
+        vm.startPrank(alice);
+        cometWrapper.mint(aliceBalance/3, alice);
+        vm.stopPrank();
+        assertEq(comet.balanceOf(address(cometWrapper)), cometWrapper.totalAssets());
+
+        assertEq(cometWrapper.userPrincipal(alice) + cometWrapper.userPrincipal(bob), uint104(comet.userBasic(address(cometWrapper)).principal));
+
+        vm.startPrank(alice);
+        cometWrapper.redeem(cometWrapper.maxRedeem(alice), alice, alice);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        cometWrapper.redeem(cometWrapper.maxRedeem(bob), bob, bob);
+        vm.stopPrank();
+    }
+
+    // Invariants:
     // - transfers must not change totalSupply
     // - transfers must not change totalAssets
     function test__transferInvariants(uint256 amount1, uint256 amount2) public {
