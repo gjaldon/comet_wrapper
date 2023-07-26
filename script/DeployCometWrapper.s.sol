502bf56ea43e238b866d4e566550eeedf2a6f443
Gabriel Jaldon <gjaldon85@gmail.com>
2023-07-26 15:16:03 +0800

    Configurable token name and symbol

--- script/DeployCometWrapper.s.sol
+++ script/DeployCometWrapper.s.sol
@@ -10,23 +10,34 @@ import {CometWrapper, CometInterface, ICometRewards, CometHelpers, ERC20} from "
 // $ forge script script/DeployCometWrapper.s.sol --rpc-url $GOERLI_RPC_URL --broadcast --verify -vvvv -t --sender address
 // Change COMET_ADDRESS and REWARDS_ADDRESS to use the correct addresses for their corresponding CHAIN. Use the correct
 // RPC too for the CHAIN you wish to deploy to.
+// Required ENV Vars:
+// COMET_ADDRESS
+// REWARDS_ADDRESS
+// TOKEN_NAME
+// TOKEN_SYMBOL
 
 contract DeployCometWrapper is Script {
-    address public cometAddr;
-    address public rewardsAddr;
+    address internal cometAddr;
+    address internal rewardsAddr;
+    string internal tokenName;
+    string internal tokenSymbol;
 
     function run() public {
         cometAddr = vm.envAddress("COMET_ADDRESS");
         rewardsAddr = vm.envAddress("REWARDS_ADDRESS");
+        tokenName = vm.envString("TOKEN_NAME");
+        tokenSymbol = vm.envString("TOKEN_SYMBOL");
         vm.startBroadcast();
 
         console.log("=============================================================");
+        console.log("Token Name:      ", tokenName);
+        console.log("Token Symbol:    ", tokenSymbol);
         console.log("Comet Address:   ", cometAddr);
         console.log("Rewards Address: ", rewardsAddr);
         console.log("=============================================================");
 
         CometWrapper cometWrapper =
-            new CometWrapper(ERC20(cometAddr), ICometRewards(rewardsAddr), "Wrapped Comet USDC", "WcUSDCv3");
+            new CometWrapper(ERC20(cometAddr), ICometRewards(rewardsAddr), tokenName, tokenSymbol);
         CometInterface comet = CometInterface(cometAddr);
 
         vm.stopBroadcast();
