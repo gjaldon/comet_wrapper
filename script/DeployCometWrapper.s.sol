71be265dccef181093bfbbf7036c064fe313c2e6
Gabriel Jaldon <gjaldon85@gmail.com>
2023-07-26 14:44:39 +0800

    Deployment for production

--- script/DeployCometWrapper.s.sol
+++ script/DeployCometWrapper.s.sol
@@ -2,21 +2,32 @@
 pragma solidity 0.8.17;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 import {CometWrapper, CometInterface, ICometRewards, CometHelpers, ERC20} from "../src/CometWrapper.sol";
 
+// Deploy with:
+// $ source .env
+// $ forge script script/DeployCometWrapper.s.sol --rpc-url $GOERLI_RPC_URL --broadcast --verify -vvvv -t --sender address
+// Change COMET_ADDRESS and REWARDS_ADDRESS to use the correct addresses for their corresponding CHAIN. Use the correct
+// RPC too for the CHAIN you wish to deploy to.
+
 contract DeployCometWrapper is Script {
-    // Goerli addresses
-    address constant COMET = 0x3EE77595A8459e93C2888b13aDB354017B198188;
-    address constant REWARDS = 0xef9e070044d62C38D2e316146dDe92AD02CF2c2c;
+    address public cometAddr;
+    address public rewardsAddr;
 
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+        cometAddr = vm.envAddress("COMET_ADDRESS");
+        rewardsAddr = vm.envAddress("REWARDS_ADDRESS");
+        vm.startBroadcast();
+
+        console.log("=============================================================");
+        console.log("Comet Address:   ", cometAddr);
+        console.log("Rewards Address: ", rewardsAddr);
+        console.log("=============================================================");
 
         CometWrapper cometWrapper =
-            new CometWrapper(ERC20(COMET), ICometRewards(REWARDS), "Wrapped Comet USDC", "WcUSDCv3");
-        CometInterface comet = CometInterface(COMET);
-        comet.allow(address(cometWrapper), true);
+            new CometWrapper(ERC20(cometAddr), ICometRewards(rewardsAddr), "Wrapped Comet USDC", "WcUSDCv3");
+        CometInterface comet = CometInterface(cometAddr);
 
         vm.stopBroadcast();
     }
